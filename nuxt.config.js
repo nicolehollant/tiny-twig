2b9965b47a6e1412b801d8181171414d2354a23c
Cole Hollant <coleghollant@gmail.com>
2020-07-16 19:50:28 -0400

    lol fix generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,11 @@ export default {
   ...modules,
   ...router,
   generate: {
-    interval: 2000
+    async routes () {
+      const { $content } = require('@nuxt/content')
+      const files = await $content().only(['path']).fetch()
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
   }
 }
